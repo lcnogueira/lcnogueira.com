5cf7501162bf5e1ceffc353e4913a6178d901d94
Luiz Claudio <luizcns18@gmail.com>
2019-11-08 18:43:18 -0300

    chore: Set algolia plugin

---
+++ src/utils/algolia_queries.js
@@ -0,0 +1,45 @@
+const postsQuery = `{
+  posts: allMarkdownRemark(
+    sort: { fields: frontmatter___date, order: DESC }
+  ) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        frontmatter {
+          background
+          category
+          date_timestamp: date
+          date(locale: "en-us", formatString: "MMMM Do YYYY")
+          description
+          title
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`;
+
+const flatten = arr =>
+  arr.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    date_timestamp: parseInt(
+      (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest,
+  }));
+
+const queries = [
+  {
+    query: postsQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: 'Posts',
+    settings: {
+      attributesToSnippet: ['excerpt:20'],
+    },
+  },
+];
+
+module.exports = queries;
