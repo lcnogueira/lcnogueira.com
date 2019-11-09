8c15ccf13c69dc37bb9238c0fe801fdb6ecf7066
Luiz Claudio <luizcns18@gmail.com>
2019-11-09 13:06:29 -0300

    feat: Add switch theme feature

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Home } from 'styled-icons/boxicons-solid/Home';
 import { SearchAlt2 as Search } from 'styled-icons/boxicons-regular/SearchAlt2';
 import { UpArrowAlt as Arrow } from 'styled-icons/boxicons-regular/UpArrowAlt';
-import { Bulb } from 'styled-icons/boxicons-regular/Bulb';
+import { Bulb as Light } from 'styled-icons/boxicons-regular/Bulb';
 import { Grid } from 'styled-icons/boxicons-solid/Grid';
 
 import * as S from './styled';
 
-const MenuBar = () => (
-  <S.MenuBarWrapper>
-    <S.MenuBarGroup>
-      <S.MenuBarLink to="/" title="Back to Home">
-        <S.MenuBarItem>
-          <Home />
+function MenuBar() {
+  const [theme, setTheme] = useState(null);
+
+  const toggleTheme = () => (theme === 'dark' ? 'light' : 'dark');
+
+  useEffect(() => {
+    setTheme(window.__theme);
+    window.__onThemeChange = () => setTheme(window.__theme);
+  }, []);
+
+  return (
+    <S.MenuBarWrapper>
+      <S.MenuBarGroup>
+        <S.MenuBarLink to="/" title="Back to Home">
+          <S.MenuBarItem>
+            <Home />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+        <S.MenuBarLink to="/search" title="Search">
+          <S.MenuBarItem>
+            <Search />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+      </S.MenuBarGroup>
+      <S.MenuBarGroup>
+        <S.MenuBarItem
+          title="Switch theme"
+          onClick={() => {
+            window.__setPreferredTheme(toggleTheme());
+          }}
+          className={theme}
+        >
+          <Light />
         </S.MenuBarItem>
-      </S.MenuBarLink>
-      <S.MenuBarLink to="/search" title="Search">
-        <S.MenuBarItem>
-          <Search />
+        <S.MenuBarItem title="Switch visualization mode">
+          <Grid />
         </S.MenuBarItem>
-      </S.MenuBarLink>
-    </S.MenuBarGroup>
-    <S.MenuBarGroup>
-      <S.MenuBarItem title="Switch theme">
-        <Bulb />
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Switch visualization mode">
-        <Grid />
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Go to the top">
-        <Arrow />
-      </S.MenuBarItem>
-    </S.MenuBarGroup>
-  </S.MenuBarWrapper>
-);
+        <S.MenuBarItem title="Go to the top">
+          <Arrow />
+        </S.MenuBarItem>
+      </S.MenuBarGroup>
+    </S.MenuBarWrapper>
+  );
+}
 
 export default MenuBar;
