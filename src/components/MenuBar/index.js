4d1c8633248ab1977cc8ac96c401a9e7225e5656
Luiz Claudio <luizcns18@gmail.com>
2019-11-09 11:16:38 -0300

    style: Add css variables

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Home } from 'styled-icons/boxicons-solid/Home';
 import { SearchAlt2 as Search } from 'styled-icons/boxicons-regular/SearchAlt2';
 import { UpArrowAlt as Arrow } from 'styled-icons/boxicons-regular/UpArrowAlt';
-import { Bulb } from 'styled-icons/boxicons-solid/Bulb';
+import { Bulb } from 'styled-icons/boxicons-regular/Bulb';
 import { Grid } from 'styled-icons/boxicons-solid/Grid';
 
 import * as S from './styled';
