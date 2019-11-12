d32ff0dad2a4a67e5c503eda48d69a578e033b34
Luiz Claudio <luizcns18@gmail.com>
2019-11-12 17:19:25 -0300

    Update metadata information

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,88 +2,90 @@ require('dotenv').config();
 
 const queries = require('./src/utils/algolia_queries');
 
+const pluginsList = [
+  `gatsby-plugin-transition-link`,
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-react-helmet`,
+  // Needs to be the first one to work with gatsby-remark-images
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `uploads`,
+      path: `${__dirname}/static/assets/img`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/posts`,
+    },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: 'gatsby-remark-relative-images',
+          options: {
+            name: 'uploads',
+          },
+        },
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            maxWidth: 960,
+            linkImagesToOriginal: false,
+          },
+        },
+        `gatsby-remark-lazy-load`,
+        `gatsby-remark-prismjs`,
+      ],
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-algolia-search`,
+    options: {
+      appId: process.env.GATSBY_ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_ADMIN_KEY,
+      indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // for all queries
+      queries,
+      chunkSize: 10000,
+      enablePartialUpdates: true, // default: false
+    },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Luiz Cláudio`,
+      short_name: `lcnogueira`,
+      start_url: `/`,
+      background_color: `#16202c`,
+      theme_color: `#16202c`,
+      display: `minimal-ui`,
+      icon: `static/assets/img/me.jpg`, // This path is relative to the root of the site.
+    },
+  },
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  // `gatsby-plugin-offline`,
+];
+
 module.exports = {
   siteMetadata: {
     title: `My personal website`,
     position: `Javascript Software Engineer`,
-    description: `My personal website where I'll show my articles, projects, talks and other stuff.`,
+    description: `My personal website where I'll show my articles, projects, experience, education and other stuff.`,
     author: `Luiz Cláudio`,
   },
-  plugins: [
-    `gatsby-plugin-transition-link`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    // Needs to be the first one to work with gatsby-remark-images
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `uploads`,
-        path: `${__dirname}/static/assets/img`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 960,
-              linkImagesToOriginal: false,
-            },
-          },
-          `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-algolia-search`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // for all queries
-        queries,
-        chunkSize: 10000,
-        enablePartialUpdates: true, // default: false
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+  plugins: pluginsList,
 };
