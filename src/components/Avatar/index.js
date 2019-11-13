2d6bf88ed60ec786f9fcd16fb6b276431b259016
Luiz Cláudio <luizcns18@gmail.com>
2019-11-12 22:51:38 -0300

    Feature/add pwa support (#16)
    * Update metadata information
    
    * feat: Enable PWA + offline functionality

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+
 import * as S from './styled';
 
 function Avatar() {
   const { avatarImage } = useStaticQuery(
     graphql`
       query {
-        avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
+        avatarImage: file(relativePath: { eq: "me.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 60) {
               ...GatsbyImageSharpFluid_tracedSVG
