616512dc11257e897b7106d918f895cac9f9b5e5
Luiz Claudio <luizcns18@gmail.com>
2019-11-12 22:50:10 -0300

    feat: Enable PWA + offline functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ const pluginsList = [
   },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
-  // `gatsby-plugin-offline`,
+  `gatsby-plugin-offline`,
 ];
 
 module.exports = {
