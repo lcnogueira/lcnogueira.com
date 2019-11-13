f0b54c56bd6c707d0cbbb3c15ece6acd1d4c41ec
Luiz Claudio <luizcns18@gmail.com>
2019-11-13 11:58:32 -0300

    Add sitemap and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ const pluginsList = [
       icon: `static/assets/img/me.jpg`, // This path is relative to the root of the site.
     },
   },
+  `gatsby-plugin-sitemap`,
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   `gatsby-plugin-offline`,
