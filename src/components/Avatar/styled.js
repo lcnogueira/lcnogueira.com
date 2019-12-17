a5635aac03639fd0c3371c5633374da397e727e1
Luiz Claudio <luizcns18@gmail.com>
2019-12-17 17:40:59 -0300

    Change avatar size and improve image quality

--- src/components/Avatar/styled.js
+++ src/components/Avatar/styled.js
@@ -4,8 +4,8 @@ import Img from 'gatsby-image';
 
 export const AvatarWrapper = styled(Img)`
   border-radius: 50%;
-  height: 3.75rem;
-  width: 3.75rem;
+  height: 8rem;
+  width: 8rem;
   margin: auto;
 
   ${media.lessThan('large')`
